9233866dcb9172d91ee37db9f683a3269304c02f
Miguel Fung <64858239+mjfung1@users.noreply.github.com>
2023-03-09 02:31:30 -0500

    build

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/", express.static("dist"))
+app.use("/", express.static("/build"))
 
 app.use("/auth", userRouter);
 app.use("/recipes", recipesRouter);
